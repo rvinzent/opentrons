63294e1a82747e255755045d7b2fdc9ac315343a
Seth Foster <seth@opentrons.com>
2018-08-17 16:20:42 -0400

    feat(api): Add robot reset endpoints and implementation to API (#1855)
    Add endpoints at GET /settings/reset/options and PUT /settings/reset to, respectively, query what
    reset options are available and reset any subset of those options to their factory configurations.
    This allows us to reset only the parts of the configuration that a user might want, and avoid
    resetting those configurations that are required for operation and difficult to change.

--- api/opentrons/server/endpoints/settings.py
+++ api/opentrons/server/endpoints/settings.py
@@ -1,8 +1,31 @@
 import logging
+import shutil
+import os
 from aiohttp import web
 from opentrons.config import advanced_settings as advs
+from opentrons.robot import robot_configs as rc
+from opentrons.data_storage import database as db
 
 log = logging.getLogger(__name__)
+
+_settings_reset_options = {
+    'deckCalibration': {
+        'name': 'Deck Calibration',
+        'description': 'Deck calibration informs the OT2 of the relative positions of the deck and the gantry. You should reset deck calibration if the OT2 is unable to properly position itself over your selected labware.' # noqa
+    },
+    'tipProbe': {
+        'name': 'Tip Probe Calibration',
+        'description': 'Tip probe calibration informs the OT2 of the length of the pipette tips it is using to aspirate reagents. You should reset tip probe calibration if the OT2 is either not inserting the pipette tip far enough into your labware,or is striking the bottom of your labware.' # noqa
+    },
+    'labwareCalibration': {
+        'name': 'Labware Calibration',
+        'description': 'Labware calibration informs the OT2 of the shape of your labware. You should reset labware calibration if the OT2 can find your labware but is interacting with the wrong part of the labware, or acting as if it is the wrong shape.' # noqa
+    },
+    'bootScripts': {
+        'name': 'Boot Scripts',
+        'description': 'Boot scripts are scripts that you may have placed in the /data/boot.d subdirectory of the persisent data store on your OT2. You should reset boot scripts if you no longer want the behavior provided by the boot scripts.' # noqa
+    }
+}
 
 
 async def get_advanced_settings(request: web.Request) -> web.Response:
@@ -43,10 +66,33 @@ async def set_advanced_setting(request: web.Request) -> web.Response:
 async def reset(request: web.Request) -> web.Response:
     """ Execute a reset of the requested parts of the user configuration.
     """
-    return web.json_response({'message': 'Not implemented'}, status=501)
+    data = await request.json()
+    for requested_reset in data.keys():
+        if requested_reset not in _settings_reset_options:
+            log.error('Bad reset option {} requested'.format(requested_reset))
+            return web.json_response(
+                {'message': '{} is not a valid reset option'
+                 .format(requested_reset)},
+                status=400)
+    log.info("Reset requested for {}".format(', '.join(data.keys())))
+    if data.get('deckCalibration'):
+        rc.clear(calibration=True, robot=False)
+    if data.get('tipProbe'):
+        config = rc.load()
+        config.tip_length.clear()
+        rc.save_robot_settings(config)
+    if data.get('labwareCalibration'):
+        db.reset()
+    if data.get('bootScripts'):
+        if os.environ.get('RUNNING_ON_PI'):
+            if os.path.exists('/data/boot.d'):
+                shutil.rmtree('/data/boot.d')
+        else:
+            log.debug('Not on pi, not removing /data/boot.d')
+    return web.json_response({}, status=200)
 
 
 async def available_resets(request: web.Request) -> web.Response:
     """ Indicate what parts of the user configuration are available for reset.
     """
-    return web.json_response({'message': 'Not implemented'}, status=501)
+    return web.json_response(_settings_reset_options, status=200)
